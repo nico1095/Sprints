4e389003eea33c374d89345f05c12d6e07df44dc
nico1095 <agiso@students.kennesaw.edu>
2025-12-10 07:25:52 -0500

    Update and rename sprint2 to sprint2.py

---
+++ sprint2.py
@@ -0,0 +1,7 @@
+
+# Program Name: Sprint1.py 
+# Course: IT3883/Section W02
+# Student Name: Anthony Giso
+# Assignment Number: Final exam
+# Due Date: 12/10/ 2025
+# Purpose:a python program that will interpret pseudo-English statements describing an amount of money in coins and convert them into a dollar amount. 
